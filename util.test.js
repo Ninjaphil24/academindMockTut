1c27cb5ec311f493a8f50c66a4788c5c97b9abe7
Ninjaphil24 <mphilippos@hotmail.com>
2022-04-30 17:21:17 +0300

    Testing with puppeteer done

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer')
 const { generateText, checkAndGenerate } = require('./util')
 
 test('should output name and age', ()=>{
@@ -15,4 +16,21 @@ test('should output name and age', ()=>{
 test('should generate a valid text output',()=>{
     const text = checkAndGenerate('Max',29)
     expect(text).toBe('Max (29 years old)')
+})
+jest.setTimeout(10000)
+test('should create an element with text and correct class',async ()=>{
+    const browser= await puppeteer.launch({
+        headless: true, // if you set to false and uncomment lines 24&25, a browser will appear
+        // slowMo: 80,
+        // args: ['--window-size=1920,1080']
+    })
+    const page=await browser.newPage()
+    await page.goto('file:///C:/javascript/academindMockTut/index.html')
+    await page.click('input#name')
+    await page.type('input#name','Anna')
+    await page.click('input#age')
+    await page.type('input#age','28')
+    await page.click('#btnAddUser')
+    const finalText=await page.$eval('.user-item', el =>el.textContent)
+    expect(finalText).toBe('Anna (28 years old)')
 })
